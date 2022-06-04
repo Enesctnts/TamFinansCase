512e0eecb51154d155eedde9d6590e5f1b906e4b
Enes Çetintaş <enesctnts@gmail.com>
2022-06-05 02:52:36 +0300

    CategoryStatus eklendi. Veritabanına bildirildi.

---
+++ TamFinansCase.DataAccess/Migrations/202206042211450_CategoryUpdate.cs
@@ -0,0 +1,18 @@
+﻿namespace TamFinansCase.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CategoryUpdate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Categories", "CategoryStatus", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Categories", "CategoryStatus");
+        }
+    }
+}
