d72c74a77bb743467d3bf8be45cb7ea6068a4092
Enes Çetintaş <enesctnts@gmail.com>
2022-06-05 13:36:39 +0300

    User tablosu düzenlendi.

---
+++ TamFinansCase.DataAccess/Migrations/202206050943523_UserUpdate.cs
@@ -0,0 +1,18 @@
+﻿namespace TamFinansCase.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserUpdate : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Users", "UserName", c => c.String(maxLength: 30));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Users", "UserName", c => c.String(nullable: false, maxLength: 30));
+        }
+    }
+}
